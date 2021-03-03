a5ff1b46cdeabaf0ff77e476e2c8cf3d0f8e7129
lucky masharane <lmasharane@gmail.com>
2021-03-03 08:27:34 +0200

    update

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Gallery } from '../gallery';
+import { GalleryService } from '../gallery.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  galleryPic: any = [];
+  constructor(private prod: GalleryService,) { }
 
   ngOnInit(): void {
+    this.getGallery();
   }
 
   pic= [
@@ -18,5 +22,17 @@ export class MainComponent implements OnInit {
     { empImage: "https://images.pexels.com/photos/3775534/pexels-photo-3775534.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-3775534.jpg&fm=jpg" },
     { empImage: "https://images.pexels.com/photos/1081188/pexels-photo-1081188.jpeg?cs=srgb&dl=pexels-graham-wizardo-1081188.jpg&fm=jpg" }
   ]
+
+  getGallery() {
+
+    return this.prod.getAllGallery().subscribe((res: any[]) => {
+      this.galleryPic = res.map(gallery => {
+        return {
+          ...gallery.payload.doc.data(),
+          id: gallery.payload.doc.id
+        } as Gallery
+      })
+    })
+  }
 
 }
