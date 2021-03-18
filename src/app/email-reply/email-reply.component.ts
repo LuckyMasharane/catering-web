ee122a4ea5ed42a8e5cc468c426f38a73258842d
lucky masharane <lmasharane@gmail.com>
2021-03-18 13:59:19 +0200

    email form

--- src/app/email-reply/email-reply.component.ts
+++ src/app/email-reply/email-reply.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+// import { AlertController, NavController } from 'ionic-angular';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailReplyComponent implements OnInit {
 
-  constructor() { }
+
+  public form!: FormGroup;
+
+  constructor(private _FORM: FormBuilder, private _EMAIL: EmailService) {
+    this.form = this._FORM.group({
+      "to": ["", Validators.required],
+      "cc": ["", Validators.required],
+      "bcc": ["", Validators.required],
+      "subject": ["", Validators.required],
+      "message": ["", Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+
+  sendMessage(): void {
+    let to: string = this.form.controls["to"].value,
+      cc: string = this.form.controls["cc"].value,
+      bcc: string = this.form.controls["cc"].value,
+      subject: string = this.form.controls["subject"].value,
+      message: string = this.form.controls["message"].value;
+
+
+    this._EMAIL.sendEmail(to, cc, bcc, subject, message);
+
   }
 
 }
